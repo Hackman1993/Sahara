7f9e3e3732e724b72819d4c24d6d06e4c8da73c1
Hackman.Lo <admin@yytec.net>
2023-07-10 14:50:56 +0800

    Wired

--- cmake/template/sahara.in.cmake
+++ cmake/template/sahara.in.cmake
@@ -3,7 +3,8 @@
 include(CMakeFindDependencyMacro)
 list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_SOURCE_DIR})
 
-find_dependency(Boost REQUIRED)
-
+find_dependency(Boost REQUIRED COMPONENTS locale)
+find_dependency(fmt REQUIRED)
+message(STATUS "BOOST:" "@Boost_FOUND@")
 if(@Boost_FOUND@)
   include_directories(${Boost_INCLUDE_DIRS})
