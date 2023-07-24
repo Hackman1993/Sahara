c2bfea13cd48ddcb7636d4eacbcf8fdd10e2fdd3
Hackman.Lo <admin@yytec.net>
2023-07-24 17:56:00 +0800

    Wired

---
+++ include/log/log.h
@@ -0,0 +1,19 @@
+//
+// Created by Hackman.Lo on 7/24/2023.
+//
+
+#ifndef SAHARA_LOG_H
+#define SAHARA_LOG_H
+#define  _WIN32_WINNT   0x0602
+#include <boost/log/trivial.hpp>
+namespace sahara {
+    namespace log {
+
+        class log {
+
+        };
+
+    } // sahara
+} // log
+
+#endif //SAHARA_LOG_H
