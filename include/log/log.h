db4c04687e89db706b7d76c8849d3732f1c3d88f
Hackman.Lo <admin@yytec.net>
2023-07-25 09:57:39 +0800

    Wired

--- include/log/log.h
+++ include/log/log.h
@@ -4,16 +4,37 @@
 
 #ifndef SAHARA_LOG_H
 #define SAHARA_LOG_H
-#define  _WIN32_WINNT   0x0602
+
+#include <boost/log/common.hpp>
+#include <boost/log/sinks.hpp>
+#include <boost/log/sources/logger.hpp>
 #include <boost/log/trivial.hpp>
-namespace sahara {
-    namespace log {
+#include <boost/log/attributes/named_scope.hpp>
 
-        class log {
+namespace sahara::log {
 
+    class log {
+        enum LoggerType {
+            console = 0, file,
         };
+        typedef boost::log::sinks::synchronous_sink<boost::log::sinks::text_file_backend> file_sink;
 
-    } // sahara
+        log() {}
+
+        ~log() {}
+
+        static log &instance();
+
+        static bool init(std::string fileName, int type, int level, int maxFileSize, int maxBackupIndex);
+
+        boost::log::sources::severity_logger<boost::log::trivial::severity_level> _logger;
+
+    };
 } // log
-
+#define LOG_TRACE(logEvent)  BOOST_LOG_FUNCTION(); BOOST_LOG_SEV(sahara::log::instance()._logger, boost::log::trivial::trace) << logEvent;
+#define LOG_DEBUG(logEvent)  BOOST_LOG_FUNCTION(); BOOST_LOG_SEV(sahara::log::instance()._logger, boost::log::trivial::debug) << logEvent;
+#define LOG_INFO(logEvent)   BOOST_LOG_FUNCTION(); BOOST_LOG_SEV(sahara::log::instance()._logger, boost::log::trivial::info) << logEvent;
+#define LOG_WARN(logEvent)   BOOST_LOG_FUNCTION(); BOOST_LOG_SEV(sahara::log::instance()._logger, boost::log::trivial::warning) << logEvent;
+#define LOG_ERROR(logEvent)  BOOST_LOG_FUNCTION(); BOOST_LOG_SEV(sahara::log::instance()._logger, boost::log::trivial::error) << logEvent;
+#define LOG_FATAL(logEvent)  BOOST_LOG_FUNCTION(); BOOST_LOG_SEV(sahara::log::instance()._logger, boost::log::trivial::fatal) << logEvent;
 #endif //SAHARA_LOG_H
