3915825aa6ae094dd8180a4cc6a94e9282cabaec
Hackman.Lo <admin@yytec.net>
2023-07-25 13:45:41 +0800

    Wired

--- cmake/template/sahara.in.cmake
+++ cmake/template/sahara.in.cmake
@@ -6,6 +6,7 @@ list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_SOURCE_DIR})
 
 find_dependency(fmt REQUIRED)
 find_dependency(date REQUIRED)
+find_dependency(spdlog REQUIRED)
 find_dependency(Boost REQUIRED COMPONENTS locale)
 
 include(${CMAKE_CURRENT_LIST_DIR}/sahara-targets.cmake)
