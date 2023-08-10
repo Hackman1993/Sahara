2afcad2b665e535a46d837a0c21d77a0779f0b01
Hackman.Lo <admin@yytec.net>
2023-08-10 23:55:48 +0800

    Fixed Exception Issue

--- test/main.cpp
+++ test/main.cpp
@@ -1,11 +1,11 @@
 //
 // Created by Hackman.Lo on 7/25/2023.
 //
-#include "include/exception/exception.h"
+#include "include/exception/database_exception.h"
 #include <iostream>
 int main(){
     try{
-        THROW(sahara::exception::database_exception,"sahara", "Wired", 200);
+        THROW(sahara::exception::database_exception,"message", "module", 200);
     }catch (sahara::exception::exception_base& e){
         std::cout << e.what() << std::endl;
     }
