19378d5a44c0d3adc4556612c7cabdb22353272d
hackman <admin@yytec.net>
2023-08-30 16:06:57 +0800

    Updated

--- test/main.cpp
+++ test/main.cpp
@@ -2,10 +2,11 @@
 // Created by Hackman.Lo on 7/25/2023.
 //
 #include "include/exception/database_exception.h"
+#include "include/exception/conversion_exception.h"
 #include <iostream>
 int main(){
     try{
-        THROW(sahara::exception::database_exception,"message", "module", 200);
+        THROW(sahara::exception::conversion_exception,"message", "module");
     }catch (sahara::exception::exception_base& e){
         std::cout << e.what() << std::endl;
     }
