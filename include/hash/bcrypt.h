c3340d0dd9c08aae7fbf29b5d536ee0aa4cac6cb
Hackman.Lo <admin@yytec.net>
2024-02-28 17:23:36 +0800

    update bcrypt

---
+++ include/hash/bcrypt.h
@@ -0,0 +1,11 @@
+#ifndef BCRYPT_H
+#define BCRYPT_H
+
+#include <string>
+
+namespace sahara::hash::bcrypt {
+    std::string generateHash(const std::string & password , unsigned rounds = 10 );
+    bool validatePassword(const std::string & password, const std::string & hash);
+}
+
+#endif // BCRYPT_H
